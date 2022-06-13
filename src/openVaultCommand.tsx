e579e35d0671864ed51bb256edd11219a1370a3a
marcjulianschwarz <info@marc-julian.de>
2022-06-13 13:15:01 +0200

    add info message to open vault command

--- src/openVaultCommand.tsx
+++ src/openVaultCommand.tsx
@@ -1,6 +1,7 @@
-import { Action, ActionPanel, closeMainWindow, List, open, popToRoot } from "@raycast/api";
+import { Action, ActionPanel, closeMainWindow, List, open, popToRoot, showToast, Toast } from "@raycast/api";
 
 import { useObsidianVaults } from "./utils/utils";
+import { NoVaultFoundMessage } from "./components/NoVaultFoundMessage";
 
 const getTarget = (vaultName: string) => {
   return "obsidian://open?vault=" + encodeURIComponent(vaultName);
@@ -15,19 +16,37 @@ export default function Command() {
     closeMainWindow();
   }
 
-  return (
-    <List isLoading={!ready}>
-      {vaults?.map((vault) => (
-        <List.Item
-          title={vault.name}
-          key={vault.key}
-          actions={
-            <ActionPanel>
-              <Action.Open title="Open vault" target={getTarget(vault.name)} />
-            </ActionPanel>
-          }
-        />
-      ))}
-    </List>
-  );
+  if (!ready) {
+    return <List isLoading={true}></List>;
+  } else if (vaults.length === 0) {
+    return <NoVaultFoundMessage />;
+  } else if (vaults.length == 1) {
+    open(getTarget(vaults[0].name));
+    popToRoot();
+    closeMainWindow();
+    return <List />;
+  } else if (vaults.length > 1) {
+    return (
+      <List isLoading={!ready}>
+        {vaults?.map((vault) => (
+          <List.Item
+            title={vault.name}
+            key={vault.key}
+            actions={
+              <ActionPanel>
+                <Action.Open title="Open vault" target={getTarget(vault.name)} />
+              </ActionPanel>
+            }
+          />
+        ))}
+      </List>
+    );
+  } else {
+    showToast({
+      title: "Path Error",
+      message: "Something went wrong with your vault path. There are no paths to select from.",
+      style: Toast.Style.Failure,
+    });
+    return <List />;
+  }
 }
