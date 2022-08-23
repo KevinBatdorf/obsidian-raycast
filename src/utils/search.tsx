3409d939ed48d1015d8e4cb0eae497203f5d2e94
marcjulianschwarz <info@marc-julian.de>
2022-08-23 21:08:37 +0200

    add search media by mentioning notes

--- src/utils/search.tsx
+++ src/utils/search.tsx
@@ -1,4 +1,5 @@
-import { Note } from "./interfaces";
+import { useNotes } from "./cache";
+import { Media, Note, Vault } from "./interfaces";
 
 export function filterNotes(notes: Note[], input: string, byContent: boolean) {
   if (input.length === 0) {
@@ -17,4 +18,24 @@ export function filterNotes(notes: Note[], input: string, byContent: boolean) {
   } else {
     return notes.filter((note) => note.title.toLowerCase().includes(input));
   }
+}
+
+export function filterMedia(mediaList: Media[], input: string, vault: Vault) {
+  if (input.length === 0) {
+    return mediaList;
+  }
+
+  input = input.toLowerCase();
+
+  let notes = useNotes(vault);
+  notes = notes.filter((note) => note.title.toLowerCase().includes(input));
+
+  return mediaList.filter((media) => {
+    return (
+      media.title.toLowerCase().includes(input) ||
+      media.path.toLowerCase().includes(input) ||
+      // Filter media that is mentioned in a note which has the searched title
+      notes.some((note) => note.content.includes(media.title))
+    );
+  });
 }
